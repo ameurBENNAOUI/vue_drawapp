9a26a697e673ca3e65010b1d24879af40675864c
mabrouk <mabrouk.bennaoui@gmail.com>
2020-04-18 16:27:10 +0100

    re design edit template

---
+++ src/views/nav.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <!-- <nav>
+        <router-link to='/'>Home</router-link>
+        <router-link to='/register'>Register</router-link>
+        <router-link to='/login'>Login</router-link>
+    </nav> -->
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Draw App</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+            <router-link  class="nav-item nav-link active" to='/'>Home <span class="sr-only">(current)</span></router-link >
+            <router-link  class="nav-item nav-link" to='/upload_template'>Upload Templates</router-link >
+            <router-link  class="nav-item nav-link" to='/view_template'>View Templates</router-link >
+            <router-link  class="nav-item nav-link disabled" to='/about'>About</router-link >
+            </div>
+        </div>
+        </nav>
+            <router-view />
+
+  </div>
+
+
+</template>
+
+<script>
+// import "font-awesome/css/font-awesome.css"
+
+export default {
+}
+</script>
